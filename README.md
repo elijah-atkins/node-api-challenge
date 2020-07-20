e5ef05e1be3436c5ef694558c03e108f7bab9914
Elijah Atkins <elijahorama@gmail.com>
2020-07-19 20:02:33 -0600

    mvp done

--- README.md
+++ README.md
@@ -52,7 +52,7 @@ Demonstrate your understanding of this Sprint's concepts by answering the follow
 
 - [x] What can the API return to help clients know if a request was successful?
 
-        With a status code 200 or 201 
+        With a status code 2xx for success(200 or 201)
 
 - [x] How can we partition our application into sub-applications?
 
@@ -65,8 +65,8 @@ Demonstrate your understanding of this Sprint's concepts by answering the follow
 
 Design and build the necessary endpoints to:
 
-- [ ] Perform CRUD operations on _projects_ and _actions_. When adding an action, make sure the `project_id` provided belongs to an existing `project`. If you try to add an action with an `id` of 3 and there is no project with that `id` the database will return an error.
-- [ ] Retrieve the list of actions for a project.
+- [x] Perform CRUD operations on _projects_ and _actions_. When adding an action, make sure the `project_id` provided belongs to an existing `project`. If you try to add an action with an `id` of 3 and there is no project with that `id` the database will return an error.
+- [x] Retrieve the list of actions for a project.
 
 Please read the following sections before implementing the Minimum Viable Product, they describe how the database is structured and the files and methods available for interacting with the data.
 
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -1,39 +1,52 @@
-const express = require('express');
+const express = require("express");
 
-const Projects = require('../data/helpers/projectModel');
+const Projects = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    // console.log(req.query);
-    Projects.get()
-      .then((project) => {
-        // note the "200" response... 2xx responses are "success" responses.
-        res.status(200).json(project);
-      })
-      .catch((error) => {
-        res.status(500).json({
-          error: "The project information could not be retrieved.",
-        });
+  // console.log(req.query);
+  Projects.get()
+    .then((project) => {
+      // note the "200" response... 2xx responses are "success" responses.
+      res.status(200).json(project);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: "The project information could not be retrieved.",
       });
-  });
+    });
+});
 
-  router.get("/:id", (req, res) => {
+router.get("/:id", (req, res) => {
+  // do your magic!
+  const { id } = req.params;
+  Projects.get(id)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: "The project information could not be retrieved.",
+      });
+    });
+});
+router.get("/:id/actions", (req, res) => {
     // do your magic!
     const { id } = req.params;
-    Projects.get(id)
-      .then((project) => {
-        res.status(200).json(project);
+    Projects.getProjectActions(id)
+      .then((actions) => {
+        res.status(200).json(actions);
       })
       .catch((error) => {
         res.status(500).json({
-          error: "The project information could not be retrieved.",
+          error: "The project action information could not be retrieved.",
         });
       });
   });
 
-  router.post('/', validateProject, (req, res) => {
-    Projects.insert(req.body)
+router.post("/", validateProject, (req, res) => {
+  Projects.insert(req.body)
     .then((project) => {
       res.status(201).json(project);
     })
@@ -42,40 +55,64 @@ router.get("/", (req, res) => {
         error: "There was an error while saving the project to the database",
       });
     });
+});
+
+router.put("/:id", validateProject, (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  Projects.update(id, changes).then((project) => {
+    if (project) {
+      res.status(201).json(changes);
+    } else {
+      res.status(500).json({
+        message: "The project could not be saved to the database",
+      });
+    }
+  });
+});
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    Projects.remove(id)
+    .then(count => {
+        res.status(200).json({ message: 'The project has been removed' });
+    }).catch(err =>{
+      res.status(500).json({ message: 'The project could not be found', error: err });
+    }
+    )
   });
 
 //custom middleware
 
+function validateProject(req, res, next) {
+  const body = req.body;
+  const { name, description } = req.body;
+  if (!body || Object.entries(body).length === 0) {
+    res.status(400).json({ message: "missing project data" });
+  }
+  if (!name || !description || name === [] || description === []) {
+    res
+      .status(400)
+      .json({ message: "missing required name and/or description field" });
+  }
+  next();
+}
 
-  
-  function validateProject(req, res, next) {
-      const body = req.body;
-      const { name, description } = req.body;
-      if (!body || Object.entries(body).length === 0) {
-        res.status(400).json({ message: "missing project data" })
-      }
-      if(!name ||!description || name === [] || description === []){
-        res.status(400).json({ message: "missing required name and/or description field" })
-      }
-        next();
-  }
-  
-  function validatePost(req, res, next) {
-    if(!req.body){
+function validatePost(req, res, next) {
+  if (!req.body) {
+    res.status(400).json({
+      message: "missing post data",
+    });
+  } else {
+    const { text } = req.body;
+    if (!text) {
       res.status(400).json({
-        message: "missing post data",
+        message: "missing required text field",
       });
-    } else {
-      const { text } = req.body
-      if(!text){
-        res.status(400).json({
-          message: "missing required text field",
-        });
-      }
     }
-  
-    next();
   }
-  
-  module.exports = router;
-  
+
+  next();
+}
+
+module.exports = router;
